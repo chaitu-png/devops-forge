70d43e362dffd2e0d6b7e372ea2193e7ced4845b
chaitu-png <schaitu1706@gmail.com>
2024-05-17 16:00:00 +0530

    fix: prevent file descriptor leaks in docker client

--- engine/container_manager.py
+++ engine/container_manager.py
@@ -2,6 +2,6 @@
 import docker
 
 def run_task(image):
-    # BUG: Resource leak - docker client not closed
-    client = docker.from_env()
-    return client.containers.run(image, detach=True)
+    # FIX: Ensure docker client is closed after use
+    with docker.from_env() as client:
+        return client.containers.run(image, detach=True)
